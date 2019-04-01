0e8be049168f5d58a79adb49d2242bd9a634ad51
Jocelyn Lim <jocelynlim@bell.net>
2019-03-31 20:46:47 -0400

    program to read and print and output input

---
+++ variables/ReadUserInput.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ReadUser {
+
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        int input = keyboard.nextInt();
+
+        System.out.println(input);
+    }
+}
